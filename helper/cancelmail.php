661865a19c9be4af136c18e5c04078397625c67c
mutosunyou <muto@muto.local>
2017-03-23 13:16:17 +0900

    ini

--- helper/cancelmail.php
+++ helper/cancelmail.php
@@ -6,8 +6,8 @@ $to=mailFromUserID($_POST['userID']);
 
 $subject = '【珈琲会員の皆様へ】';
 
-$message = 'コーヒー会員の方に連絡しています。'.PHP_EOL.PHP_EOL;
-$message.= '今月の'.PHP_EOL;
+$message.= '今月の支払状況が支払済から未払いに戻されました。'.PHP_EOL;
+$message.= '意図した状態でなければ集金係の方にご確認ください。'.PHP_EOL;
 $message.= 'http://192.168.100.209/coffee/index.php'.PHP_EOL.PHP_EOL;
 
 $headers = 'remote_manager@sunyou.co.jp';
--- helper/confirm.php
+++ helper/confirm.php
@@ -15,10 +15,10 @@ for($i=0;$i<count($rst);$i++){
 
 $subject = '【珈琲会員の皆様へ】';
 
-$message = 'コーヒー会員の方に連絡しています。'.PHP_EOL.PHP_EOL;
 $message.= '来月コーヒーサーバーを利用したい方は、今月中に会員ページの表示を「来月利用する」にしてください。'.PHP_EOL;
+$message.= '利用しない方は「来月利用しない」にしてください。'.PHP_EOL;
 $message.= 'http://192.168.100.209/coffee/index.php'.PHP_EOL.PHP_EOL;
-$message.= '来月1日9時の時点で利用者を決定します。'.PHP_EOL.PHP_EOL;
+$message.= '来月1日9時の時点で利用者は確定されます。'.PHP_EOL.PHP_EOL;
 
 $headers = 'remote_manager@sunyou.co.jp';
 
