540b36f1d1724cc98b45d398c957aa0b52168ab2
mutosunyou <muto@muto.local>
2017-03-23 13:50:33 +0900

    ini

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ $body.='</h2>';
 //トグル
 $body.='<div class="well" style="height:200px;width:750px;">';
 if(isset($rst[0]['next'])!=0){
-  $body.='<div id="exp" style="float:left;display:inline-block;margin:0 0 0 10px;"></div>';
+  $body.='<div id="exp" style="display:inline-block;margin:0 0 0 10px;"></div>';
   $body.='<div id="toggle" class="toggle-iphone">';
   $body.='<div class="toggle toggle-select" data-type="select" data-toggle-on=';
   if($next=="true"){
