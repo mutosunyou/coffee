a4e23afbbf9bcb62b1eb2c90520286faefd53083
mutosunyou <muto@muto.local>
2017-03-29 13:43:09 +0900

    リストに全メンバー追加

--- lister.php
+++ lister.php
@@ -30,27 +30,37 @@ for($i=0;$i<count($rst);$i++){//指定されたuserIDのデータ全て
   $rst_check=selectData(DB_NAME,$sql);  
   $sql='select * from member where userID='.$_SESSION['loginid'].' and voted=1';
   $rst_vote=selectData(DB_NAME,$sql);
+  if($rst[$i]['available']==1){
+    if($rst_check[0]['paydate']==null){
+      $body.='未払い';
+    }else{
+      $body.='支払済';
+    }
+  }
+  $body .= '</td>';
+
+  $body .= '<td style="width:120px;nowrap;">';
   if($rst_vote[0]['voted']==1){
     if($rst_check[0]['paydate']==null){
       $body .= '<button name="'.$rst[$i]['userID'].'" class="havepay btn btn-success btn-xs">支払済</button>';
     }else{
       $body .= '<button name="'.$rst_check[0]['id'].'" user="'.$rst_check[0]['userID'].'" class="paycancel btn btn-default btn-xs">キャンセル</button>';
     }
-  }
-  $body .= '</td>';
-
-  $body .= '<td style="width:120px;nowrap;">';
-  if($rst_check!=null){
-    $body .= substr($rst_check[0]['paydate'],-2).'日(済)';
+  }else{
+    if($rst_check!=null){
+      $body .= substr($rst_check[0]['paydate'],-2).'日(済)';
+    }
   }
   $body .= '</td>';
 
   $sql='select * from checked where paydate >= "'.date('Y-m-',strtotime(date('Y-m-1').'-1 month')).'01" and paydate <= "'.date('Y-m-',strtotime(date('Y-m-1').'-1 month')).'31" and userID='.$rst[$i]['userID'];
   $rst2=selectData(DB_NAME,$sql);
   $body .= '<td>';
+
   if($rst2!=null){
     $body .= substr($rst2[0]['paydate'],-2).'日(済)';
   }
+
   $body .= '</td>';
 
   $sql='select * from checked where paydate >= "'.date('Y-m-',strtotime(date('Y-m-1').'-2 month')).'01" and paydate <= "'.date('Y-m-',strtotime(date('Y-m-1').'-2 month')).'31" and userID='.$rst[$i]['userID'];
@@ -71,6 +81,7 @@ for($i=0;$i<count($rst);$i++){//指定されたuserIDのデータ全て
   $sql='select * from checked where paydate >= "'.date('Y-m-').'01" and paydate <= "'.date('Y-m-').'31" and userID='.$rst[$i]['userID'];
   $rst_check=selectData(DB_NAME,$sql);  
 
+    $body.='利用なし';
   $body .= '</td>';
 
   $body .= '<td style="width:120px;nowrap;">';
